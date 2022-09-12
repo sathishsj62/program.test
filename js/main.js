10b6500f52bed49549224c47c19ded200bcc5a66
SathishKumar <sathishvivis6620@gmail.com>
2022-09-12 18:07:59 +0530

    modified:index.html

--- js/main.js
+++ js/main.js
@@ -1,123 +1,213 @@
 // random color//
 
-function randomcolor() {
-    var x = Math.floor(Math.random() * 100);
-    var y = Math.floor(Math.random() * 150);
-    var z = Math. floor(Math.random() * 256 );
-    var randomcolor = "rgb(" + x + "," + y + ","+z+")";
+// function randomcolor() {
+//     var x = Math.floor(Math.random() * 100);
+//     var y = Math.floor(Math.random() * 150);
+//     var z = Math. floor(Math.random() * 256 );
+//     var randomcolor = "rgb(" + x + "," + y + ","+z+")";
 
-    console.log(randomcolor);
+//     console.log(randomcolor);
 
 
-}
+// }
 
-randomcolor();
+// randomcolor();
 
 // random num //
 
-function randomnum() {
-    let a = Math.floor(Math.random() * 60)
-    let b = Math.floor(Math.random() * 100)
-    let c = Math.floor(Math.random() * 150)
-    let randomnum = "num(" + a + "," + b + "," + c + ")";
-    console.log(randomnum);
-}
-randomnum();
+// function randomnum() {
+//     let a = Math.floor(Math.random() * 60)
+//     let b = Math.floor(Math.random() * 100)
+//     let c = Math.floor(Math.random() * 150)
+//     let randomnum = "num(" + a + "," + b + "," + c + ")";
+//     console.log(randomnum);
+// }
+// randomnum();
 
 
 
 /// Given two strings, write a program that finds which string has more vowels //
 
-function x(a, b) {
-    let c = 0;
-    for (i = 0; i < a.length; i++) {
-        for (j = 0; j < b.length; j++) {
-            if (a[i] == b[j])
-                c++
-        }
-    }
-    return c;
-}
-let m, h;
-let f = prompt("enter the first string");
-let s = prompt("enter the second string");
-let v = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
-m = x(f, v);
-h = x(s, v);
-if (m == h) {
-    console.log("the both string has same vowels");
-} else if (m > h) {
-    console.log("the first string has more vowels");
-} else {
-    console.log("the second string has more vowels");
-}
+// function x(a, b) {
+//     let c = 0;
+//     for (i = 0; i < a.length; i++) {
+//         for (j = 0; j < b.length; j++) {
+//             if (a[i] == b[j])
+//                 c++
+//         }
+//     }
+//     return c;
+// }
+
+// let m, h;
+// let f = prompt("enter the first string");
+// let s = prompt("enter the second string");
+// let v = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+// m = x(f, v);
+// h = x(s, v);
+// if (m == h) {
+//     console.log("the both string has same vowels");
+// } else if (m > h) {
+//     console.log("the first string has more vowels");
+// } else {
+//     console.log("the second string has more vowels");
+// }
 
 // how many vowels in name to printe the num//
 
-let x = "sathishkumar";
-let y = ['a', 'e', 'i', 'o', 'u']
-let sum = 0
-for (i = 0; i < x.length; i++) {
-    if (y.includes(x[i])) {
-        sum += 1;
-    }
-}
-console.log(sum);
+// let x = "sathishkumar";
+// let y = ['a', 'e', 'i', 'o', 'u']
+// let sum = 0
+// for (i = 0; i < x.length; i++) {
+//     if (y.includes(x[i])) {
+//         sum += 1;
+//     }
+// }
+// console.log(sum);
 
 //  Print numbers in words in reverse order last  num///
 
-function v(num,j){
-    var str = Math.floor(num%10);
-    for(let i = 0; i < j.length; i++){
-        if(str == i){
-            console.log(j[i]);
-        }
-    }
-    }
-    var n = 453677;
-    let j=["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten"];
-       v(n,j);
+// function v(num,j){
+//     var str = Math.floor(num%10);
+//     for(let i = 0; i < j.length; i++){
+//         if(str == i){
+//             console.log(j[i]);
+//         }
+//     }
+//     }
+//     var n = 453677;
+//     let j=["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten"];
+//        v(n,j);
 
 //  Print numbers in words in reverse order//
 
-let n=["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten"];
-   let x='845';
-   let str=""; 
-   for(i=x.length-1;i>=0;i--){
-      str+=x[i]
-   }
-   let c = "";
-   for(j=0;j<str.length;j++){
-       for(k=0;k<n.length;k++){
-           if(str[j]==k){
-			   c+=n[k];
-           }
-       }
+// let n=["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten"];
+//    let x='845';
+//    let str=""; 
+//    for(i=x.length-1;i>=0;i--){
+//       str+=x[i]
+//    }
+//    let c = "";
+//    for(j=0;j<str.length;j++){
+//        for(k=0;k<n.length;k++){
+//            if(str[j]==k){
+// 			   c+=n[k];
+//            }
+//        }
        
-   }  
-   console.log(c)
+//    }  
+//    console.log(c)
 
-///Given an array with numbers, write a program that prints nearest larger value of each index position//
+// ///Given an array with numbers, write a program that prints nearest larger value of each index position//
 
-let n = prompt("enter the number");
-  let a = [1,2,3,4,5,6,7,8,9,10,30];
-  for(i=0 ;i<a.length; i++){
-	  if(n<a[i]){
-		  console.log(a[i]);
-		  break;
-	  }
-  }
+// let n = prompt("enter the number");
+//   let a = [1,2,3,4,5,6,7,8,9,10,30];
+//   for(i=0 ;i<a.length; i++){
+// 	  if(n<a[i]){
+// 		  console.log(a[i]);
+// 		  break;
+// 	  }
+//   }
 
-
-
-  let x='kumaresanmpi@gmail.com';
-  let y=''
-  for(i=0;i<x.length;i++){
-      if(x[i]=='@'){
-          continue;
+function small_large(grt){
+   let t =-1;
+   grt=grt.sort(function(a,b){returna-b})
+   if(grt==""){
+      console.log(t);
+   }else{
+      console.log(grt[0]);
+   }
+}
+let a= [10,8,11,20,15]
+let great=[];
+for(i=0;i<a.length;i++){
+   for(j=0;j<a.length;j++){
+      if(a[i]<a[j]){
+         great.push(a[j]);
       }
-      y+=x[i]
-  }
-  console.log(y)
-  
+   }
+   small_large(great);
+   great=[];
+}
 
+// split @ in mail id //
+
+//   let x='sathishviviis6620@gmail.com';
+//   let y=''
+//   for(i=0;i<x.length;i++){
+//       if(x[i]=='@'){
+//           continue;
+//       }
+//       y+=x[i]
+//   }
+//   console.log(y)
+
+// using the inbuild method//
+
+//   let  email="sathishvivis6620@gmail.com"
+//   let str = email.indexOf("@")
+//   console.log("str",str)
+//   let k = email.slice(0,str);
+//   let name = k.match(/[a-z]/g);
+//   let num  = k.match(/[0-9]/g);
+//   console.log(name);
+//   console.log(num)
+  
+ /*  let x='sathishvivis6620@gmail.com';
+  let y=''
+   for(i=0;i<x.length;i++){
+    if(x[i]=='@'){
+        continue;
+    }
+    y+=x[i]
+ }
+  console.log(y)
+ */
+
+ /// Write a program to calculate age using JS date///
+
+  function calculateage(age) { 
+    var x = Date.now() -age.getTime();
+    console.log(x);
+    var y = new Date(x); 
+    console.log(y);
+  
+    return Math.abs(y.getFullYear() -1970);
+}
+console.log(calculateage(new Date(1998, 06, 02))); 
+
+
+let x = new Date();
+ let y  = prompt("enter the year")
+ let z = x.getFullYear();
+ let m = z-y ;
+ console.log(m);
+
+
+ ///Write a program to Check whether a date is a weekend date or not///
+
+var date=new Date("2022-09-11");
+var Day = date.getDay();
+// console.log(Day);
+var Weekend = (Day === 6) || (Day === 0);
+ Weekend?console.log('weekend'):console.log('not weekend')
+
+//
+
+var date=new Date("2022-09-12");
+var Day = date.getDay();
+var Weekend = (Day === 6) || (Day === 0);
+if(Weekend){
+   console.log("a Weekend");
+} else {
+   console.log(" a not a Weekend");
+}
+
+
+function age(birthYear){
+let today = new Date();
+let bronDate = new Date(birthYear);
+let myAge=today.getFullYear() - bronDate.getFullYear();
+
+}
+console.log(age('1998-06-02'));
