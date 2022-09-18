98d85523aa050eac6b6d0beed99fc9c3011348f4
SathishKumar <sathishvivis6620@gmail.com>
2022-09-18 23:29:52 +0530

    modified:index.html

--- programs test.js/vowels.js
+++ programs test.js/vowels.js
@@ -42,3 +42,35 @@
 //     }
 // }
 // console.log(sum);
+
+
+// document//
+
+// function x(a, b) {
+//    let c = 0;
+//    for (i = 0; i < a.length; i++) {
+//        for (j = 0; j < b.length; j++) {
+//            if (a[i] == b[j])
+//                c++
+//        }
+//    }
+//    return c;
+// }
+
+
+// let m, h;
+// let f = prompt("enter the first string");
+// let s = prompt("enter the second string");
+// let v = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+// m = x(f, v);
+// h = x(s, v);
+// if (m == h) {
+//    // console.log("the both string has same vowels");
+//    document.getElementById('heading').innerHTML='the both string are same vowels';
+// } else if (m > h) {
+//    console.log("the first string has more vowels");
+//    document.getElementById('heading').innerHTML='the first string has more vowels';
+// } else {
+//    console.log("the second string has more vowels");
+//    document.getElementById('heading').innerHTML='the second string has more vowels';
+// }
