bddd7f78bcfe7bcb0dfd19e3bb2ed8d99fad27f4
SathishKumar <sathishvivis6620@gmail.com>
2022-09-18 23:26:36 +0530

    modified:index.html

--- programs test.js/vowels.js
+++ programs test.js/vowels.js
@@ -2,43 +2,43 @@
 
 // Given two strings, write a program that finds which string has more vowels //
 
-function x(a, b) {
-    let c = 0;
-    for (i = 0; i < a.length; i++) {
-        for (j = 0; j < b.length; j++) {
-            if (a[i] == b[j])
-                c++
-        }
-    }
-    return c;
-}
+// function x(a, b) {
+//     let c = 0;
+//     for (i = 0; i < a.length; i++) {
+//         for (j = 0; j < b.length; j++) {
+//             if (a[i] == b[j])
+//                 c++
+//         }
+//     }
+//     return c;
+// }
 
 
-let m, h;
-let f = prompt("enter the first string");
-let s = prompt("enter the second string");
-let v = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
-m = x(f, v);
-h = x(s, v);
-if (m == h) {
-    // console.log("the both string has same vowels");
-    document.getElementById('heading').innerHTML='the both string are same vowels';
-} else if (m > h) {
-    console.log("the first string has more vowels");
-    document.getElementById('heading').innerHTML='the first string has more vowels';
-} else {
-    console.log("the second string has more vowels");
-    document.getElementById('heading').innerHTML='the second string has more vowels';
-}
+// let m, h;
+// let f = prompt("enter the first string");
+// let s = prompt("enter the second string");
+// let v = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+// m = x(f, v);
+// h = x(s, v);
+// if (m == h) {
+//     // console.log("the both string has same vowels");
+//     document.getElementById('heading').innerHTML='the both string are same vowels';
+// } else if (m > h) {
+//     console.log("the first string has more vowels");
+//     document.getElementById('heading').innerHTML='the first string has more vowels';
+// } else {
+//     console.log("the second string has more vowels");
+//     document.getElementById('heading').innerHTML='the second string has more vowels'
+// }
 
 // how many vowels in name to printe the num//
 
-let x = "sathishkumar";
-let y = ['a', 'e', 'i', 'o', 'u']
-let sum = 0
-for (i = 0; i < x.length; i++) {
-    if (y.includes(x[i])) {
-        sum += 1;
-    }
-}
-console.log(sum);
+// let x = "sathishkumar";
+// let y = ['a', 'e', 'i', 'o', 'u']
+// let sum = 0
+// for (i = 0; i < x.length; i++) {
+//     if (y.includes(x[i])) {
+//         sum += 1;
+//     }
+// }
+// console.log(sum);
