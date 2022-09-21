58b2e0e7fde166ba3719f19a738367acb6a900e7
SathishKumar <sathishvivis6620@gmail.com>
2022-09-21 13:22:11 +0530

    modified:index.html

--- programs test.js/special characters.js
+++ programs test.js/special characters.js
@@ -1,6 +1,13 @@
 //special characters//
 
-let s ="sathish@12345 $%^&*!@"
+let s ="sathish@12345 SATHISH  $%^&*!@"
 let a = s.replace(/[^A-Za-z0-9]/g,"");
-console.log(a);
-document.getElementById("characters").innerHTML=a;
+let b = s.replace(/[^a-z]/g,"");
+let c = s.replace(/[^A-Z]/g,"");
+let d = s.replace(/[^0-9]/g,"");
+let e = s.replace(/[A-Za-z0-9]/g,"");
+
+document.write(a,b,c,d,e);
+// document.write(b);
+// document.write(c);
+// document.write(d);
